a6876dbb49d749dff2147ab6b2050f8089b0979f
PC-Checker <johnnycox2415@gmail.com>
2025-10-15 04:10:08 -0500

    Add files via upload

---
+++ dasda.bat
@@ -0,0 +1,9 @@
+@echo off
+color 4f
+mode con: cols=60 lines=8
+echo.
+echo  ===========================================
+echo   !!!  YOU GOT HACKED!  !!!
+echo  ===========================================
+echo.
+pause
